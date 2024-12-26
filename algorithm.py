9597965bef8d46d60c11a43efc87b4547e15e10e
Squirrel <2200013065@stu.pku.edu.cn>
2024-12-26 10:03:32 +0800

    debug

--- algorithm.py
+++ algorithm.py
@@ -2,8 +2,9 @@ import argparse
 import numpy as np
 
 args = argparse.ArgumentParser()
-args.add_argument('--n'        , type = int   , default = 2       )
+args.add_argument('--n'        , type = int   , default = 4       )
 args.add_argument('--epoch'    , type = int   , default = 1000000 )
+args.add_argument('--try_time' , type = int   , default = 100     )
 args.add_argument('--mu_0'     , type = float , default = 0.005   )
 args.add_argument('--nu'       , type = float , default = 200     )
 args.add_argument('--mod'      , type = int   , default = 100     )
@@ -34,11 +35,13 @@ def construct_lattice(n):
         z = URAN([n])
         y = z - CLP(matrix, z @ matrix)
         e = y @ matrix
+        print(e)
         e_2norm = np.linalg.norm(e) ** 2 # squared 2-norm
-        for i in range(1, n + 1):
-            for j in range(1, n):
+        print(e_2norm)
+        for i in range(n):
+            for j in range(i):
                 matrix[i][j] -= mu * y[i] * e[j]
-            matrix[i][i] -= mu * (y[i] * e[j] - e_2norm / (n * matrix[i][i]))
+            matrix[i][i] -= mu * (y[i] * e[i] - e_2norm / (n * matrix[i][i]))
         if t % args.mod == args.mod - 1:
             matrix = np.linalg.cholesky(RED(matrix))
             v = np.prod([matrix[i][i] for i in range(n)])
@@ -49,9 +52,12 @@ def NSM(matrix, n):
     pass
 
 if __name__ == '__main__':
-    for i in range():
+    for i in range(args.try_time):
         status, matrix = construct_lattice(args.n)
         if status:
-            print('Lattice:\n, fail_time:'.format(matrix, i))
+            print('Lattice:{}\n, try_time:{}'.format(matrix, i))
             break
-    
+        if i == args.try_time - 1:
+            print('Fail to construct a lattice after {} times'.format(args.try_time))
+            exit(0)
+    print('Loss:{}'.format(NSM(matrix, args.n)))
