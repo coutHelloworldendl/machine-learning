cb809165e9ef88f70a9ab15e9d89a1bbf21c1410
SwParallel <1986554825@qq.com>
2025-01-03 18:34:25 +0800

    format

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def construct_lattice(n, f):
             v = np.prod([matrix[i][i] for i in range(n)])
             matrix = matrix * pow(v, -1/n)
         if (t + 1) % args.dbg_epoch == 0:
-            f.write('Epoch {}: matrix = {}\n'.format(t + 1, matrix))
+            f.write('Epoch {}: matrix = \n{}\n'.format(t + 1, matrix))
     return True, matrix
 
 if __name__ == '__main__':
