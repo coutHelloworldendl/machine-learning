2bcc8983511d5ba7d4a85f53f52f957fa5bc8d1f
Squirrel <2200013065@stu.pku.edu.cn>
2025-01-03 10:49:45 +0800

    update

--- utils/args.py
+++ utils/args.py
@@ -6,14 +6,16 @@ Use 'from utils.args import args' to import the arguments.
 
 import argparse
 
-args = argparse.ArgumentParser()
-args.add_argument('--n'        , type = int   , default = 3           ) # lattice dimension
-args.add_argument('--epoch'    , type = int   , default = 1000000     ) # number of iterations
-args.add_argument('--dbg_epoch', type = int   , default = 10000       ) # number of iterations between two consecutive debug information
-args.add_argument('--try_time' , type = int   , default = 100         ) # number of tries
-args.add_argument('--mu_0'     , type = float , default = 0.005       ) # initial learning rate
-args.add_argument('--delta'    , type = float , default = 0.75        ) # LLL parameter
-args.add_argument('--nu'       , type = float , default = 200         ) # decay rate
-args.add_argument('--mod'      , type = int   , default = 100         ) # number of iterations between two consecutive lattice reduction
-args.add_argument('--log'      , type = str   , default = './log'     ) # log file
-args = args.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--n'        , type = int   , default = 5           ) # lattice dimension
+parser.add_argument('--epoch'    , type = int   , default = 1000000     ) # number of iterations
+parser.add_argument('--dbg_times', type = int   , default = 100         ) # number of debug information
+parser.add_argument('--try_time' , type = int   , default = 100         ) # number of tries
+parser.add_argument('--mu_0'     , type = float , default = 0.005       ) # initial learning rate
+parser.add_argument('--delta'    , type = float , default = 0.75        ) # LLL parameter
+parser.add_argument('--nu'       , type = float , default = 200         ) # decay rate
+parser.add_argument('--mod'      , type = int   , default = 100         ) # number of iterations between two consecutive lattice reduction
+parser.add_argument('--log'      , type = str   , default = './log'     ) # log file
+args = parser.parse_args()
+parser.add_argument('--dbg_epoch', type = int   , default = args.epoch / args.dbg_times ) # number of iterations between two consecutive debug information
+args = parser.parse_args()
